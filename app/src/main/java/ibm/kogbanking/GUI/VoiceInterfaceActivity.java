4cf093cb9751c461d811fc71541e18c174c04334
Len <len13@hotmail.de>
2016-11-26 17:08:31 +0100

    coke

--- app/src/main/java/ibm/kogbanking/GUI/VoiceInterfaceActivity.java
+++ app/src/main/java/ibm/kogbanking/GUI/VoiceInterfaceActivity.java
@@ -2,8 +2,16 @@ package ibm.kogbanking.GUI;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+
+
+import java.util.ArrayList;
 
 import ibm.kogbanking.R;
 
@@ -13,6 +21,11 @@ import ibm.kogbanking.R;
 
 public class VoiceInterfaceActivity extends Activity {
     ConversationService service;
+    ListView messages;
+    EditText aMsg;
+    Button send;
+    ArrayList<String> conversation;
+    ArrayAdapter<String> adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,23 @@ public class VoiceInterfaceActivity extends Activity {
 
         service = new ConversationService("2016-09-20");
         service.setUsernameAndPassword("{ORqPn3GxwWDu}", "{778fd675-2f27-422c-8599-8528a8766890}");
+
+        send = (Button) findViewById(R.id.sendBtn);
+        aMsg = (EditText) findViewById(R.id.messageTxt);
+        messages = (ListView) findViewById(R.id.exchangedMsgs);
+        conversation = new ArrayList<>();
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = aMsg.getText().toString();
+                conversation.add(msg);
+            }
+        });
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, conversation);
+        
+
 
     }
 }
