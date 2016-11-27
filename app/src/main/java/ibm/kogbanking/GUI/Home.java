0a50934236f2af829df41a159782c0eedc84aadb
iBuaRecords <daywalker@pferr.de>
2016-11-27 07:13:45 +0100

    Packages erstellt

--- app/src/main/java/ibm/kogbanking/GUI/Home.java
+++ app/src/main/java/ibm/kogbanking/GUI/Home.java
@@ -2,6 +2,7 @@ package ibm.kogbanking.GUI;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +12,10 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -21,6 +25,10 @@ import java.util.Locale;
 import ibm.kogbanking.CustomAdapter;
 import ibm.kogbanking.R;
 import android.speech.tts.TextToSpeech;
+
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
 
 /**
  * Created by Ludwig on 26.11.2016.
@@ -40,6 +48,8 @@ public class Home extends Activity implements android.speech.tts.TextToSpeech.On
         setContentView(R.layout.activity_home);
 
         tts = new android.speech.tts.TextToSpeech(this, this);
+
+        watsonSpeech();
 
         account = new ArrayList<String[]>();
         String[] test1 = {"Dr. Koch","Arztrechnung", "234", "25.11.16"};
@@ -109,7 +119,7 @@ public class Home extends Activity implements android.speech.tts.TextToSpeech.On
                     || result == android.speech.tts.TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("TTS", "This Language is not supported");
             } else {
-                speakOut("Willkommen");
+                //speakOut("Willkommen");
             }
 
         } else {
@@ -119,6 +129,54 @@ public class Home extends Activity implements android.speech.tts.TextToSpeech.On
 
     private void speakOut(String text) {
 
-        tts.speak(text, android.speech.tts.TextToSpeech.QUEUE_FLUSH, null);
+        //tts.speak(text, android.speech.tts.TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void watsonSpeech(){
+        com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech service = new com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech();
+        service.setUsernameAndPassword("{username}", "{password}");
+
+        try {
+            String text = "Hello world.";
+            ServiceCall<InputStream> stream = service.synthesize(text, Voice.EN_ALLISON);
+
+            InputStream in = WaveUtils.reWriteWaveHeader(stream.execute());
+            OutputStream out = new FileOutputStream("hello_world.wav");
+            byte[] buffer = new byte[1024];
+
+
+                try
+                {
+
+                    File path=new File(getCacheDir()+"/musicfile.3gp");
+
+                    FileOutputStream fos = new FileOutputStream(path);
+                    fos.write(buffer);
+                    fos.close();
+
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+
+                    FileInputStream fis = new FileInputStream(path);
+                    mediaPlayer.setDataSource(getCacheDir()+"/musicfile.3gp");
+
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                }
+                catch (IOException ex)
+                {
+                    String s = ex.toString();
+                    ex.printStackTrace();
+                }
+
+
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
